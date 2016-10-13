caff46c602c923209f969be980cf57939b2e06c8
yjimenezbe <yjimenezbe@gmail.com>
2016-10-13 22:52:48 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {// test
 	// write your code here
         //jkhjkknhkjh
 //jkhjkknhkjh
